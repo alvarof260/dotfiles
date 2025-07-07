a8c6692a54ad2687d2ee2afea9efac71dddc7597
Alvaro Estanislao Figueroa <alvarof260@gmail.com>
2025-07-07 19:49:37 -0300

    agrega comandos de video, audio y lanzar app

--- qtile/settings/keys.py
+++ qtile/settings/keys.py
@@ -6,18 +6,25 @@ mod = "mod4"
 terminal = guess_terminal()
 
 keys = [
-    # A list of available commands that can be bound to keys can be found
-    # at https://docs.qtile.org/en/latest/manual/config/lazy.html
-    # Switch between windows
+    # Switch between windows in current stack pane
     Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+    Key(
+        [mod],
+        "space",
+        lazy.layout.next(),
+        desc="Move window focus to other window",
+    ),
+
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
     Key(
-        [mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"
+        [mod, "shift"],
+        "h",
+        lazy.layout.shuffle_left(),
+        desc="Move window to the left",
     ),
     Key(
         [mod, "shift"],
@@ -27,11 +34,20 @@ keys = [
     ),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
-    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
     Key(
-        [mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"
+        [mod, "control"],
+        "h",
+        lazy.layout.grow_left(),
+        desc="Grow window to the left",
+    ),
+    Key(
+        [mod, "control"],
+        "l",
+        lazy.layout.grow_right(),
+        desc="Grow window to the right",
     ),
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
@@ -65,8 +81,54 @@ keys = [
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
     Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
-    # Captura pantalla completa y guarda en ~/Pictures
-    Key([], "Print", lazy.spawn("scrot ~/Pictures/%Y-%m-%d-%H%M%S.png")),
-    # Captura región seleccionada
-    Key(["shift"], "Print", lazy.spawn("scrot -s ~/Pictures/%Y-%m-%d-%H%M%S.png")),
+    Key(
+        [mod],
+        "Print",
+        lazy.spawn("scrot ~/Pictures/%Y-%m-%d-%H%M%S.png"),
+        desc="Take screenshot fullscreen",
+    ),
+    Key(
+        [mod, "shift"],
+        "Print",
+        lazy.spawn("scrot -s ~/Pictures/%Y-%m-%d-%H%M%S.png"),
+        desc="Take screenshot custom",
+    ),
+    Key([mod], "v", lazy.spawn("alacritty -e nvim"), desc="Open nvim"),
+    Key([mod], "b", lazy.spawn("zen-browser"), desc="Open browser"),
+    Key(
+        [],
+        "F3",
+        lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ +5%"),
+        desc="Raise volume by 5%",
+    ),
+    Key(
+        [],
+        "F2",
+        lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ -5%"),
+        desc="Lower volume by 5%",
+    ),
+    Key(
+        [],
+        "F4",
+        lazy.spawn("pactl set-sink-mute @DEFAULT_SINK@ toggle"),
+        desc="Mute/Unmute volume",
+    ),
+    Key(
+        [mod],
+        "F5",
+        lazy.spawn("playerctl stop"),
+        desc="Stop video",
+    ),
+    Key(
+        [mod],
+        "F6",
+        lazy.spawn("playerctl previous"),
+        desc="Previous video",
+    ),
+    Key(
+        [mod],
+        "F8",
+        lazy.spawn("playerctl next"),
+        desc="Next video",
+    ),
 ]
