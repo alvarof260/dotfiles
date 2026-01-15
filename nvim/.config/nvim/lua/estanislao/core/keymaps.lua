b033b1c52e65250c39ee7de1c51378e7a54386d0
Alvaro Estanislao Figueroa <alvarof260@gmail.com>
2026-01-15 13:13:18 -0300

    feat: remapear keymaps para dimesionar windows

--- nvim/.config/nvim/lua/estanislao/core/keymaps.lua
+++ nvim/.config/nvim/lua/estanislao/core/keymaps.lua
@@ -144,3 +144,8 @@ vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to left window", remap = tru
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", remap = true})
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", remap = true})
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true})
+
+vim.keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
