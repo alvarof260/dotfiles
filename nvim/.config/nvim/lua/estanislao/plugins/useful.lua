3c0da3b8ae4555d68ba04211cfe4651ec61dc6a5
Alvaro Estanislao Figueroa <alvarof260@gmail.com>
2026-01-17 18:16:25 -0300

    feat: lazydev para arreglar problema de uso de vim.

--- nvim/.config/nvim/lua/estanislao/plugins/useful.lua
+++ nvim/.config/nvim/lua/estanislao/plugins/useful.lua
@@ -1,4 +1,16 @@
 return {
     "nvim-lua/plenary.nvim",
     "alexghergh/nvim-tmux-navigation",
+    {
+        "folke/lazydev.nvim",
+        ft = "lua",
+        opts = {
+            library = {
+                {
+                    path = "${3rd}/plenary.nvim/lua",
+                    words = { "plenary" }
+                },
+            },
+        },
+    }
 }
