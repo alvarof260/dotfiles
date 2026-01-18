2c007bb0dbc9236f7cd019945bbb135e010e425c
Alvaro Estanislao Figueroa <alvarof260@gmail.com>
2026-01-17 21:30:39 -0300

    refactor: autopairs y autotag con blink

---
+++ nvim/.config/nvim/lua/estanislao/plugins/autopairs-autotag.lua
@@ -0,0 +1,33 @@
+return {
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        config = function()
+            local autopairs = require("nvim-autopairs")
+
+            autopairs.setup({
+                check_ts = true,
+                ts_config = {
+                    lua = { "string" },
+                    javascript = {"template_string"},
+                    java = false,
+                },
+            })
+        end,
+    },
+    {
+        "windwp/nvim-ts-autotag",
+        event = "InsertEnter",
+        config = function ()
+            local nvim_ts_autotag = require("nvim-ts-autotag")
+
+            nvim_ts_autotag.setup({
+                opts = {
+                    enable_close = true,
+                    enable_rename = true,
+                    enable_close_on_slash = false,
+                }
+            })
+        end
+    }
+}
