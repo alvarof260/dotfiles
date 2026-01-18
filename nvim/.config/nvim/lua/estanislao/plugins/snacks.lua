69a081e59cd9751769d81a44473e8c8e9a61e1a6
Alvaro Estanislao Figueroa <alvarof260@gmail.com>
2026-01-18 11:45:51 -0300

    feat: agrega lineas de indentacion para lectura

--- nvim/.config/nvim/lua/estanislao/plugins/snacks.lua
+++ nvim/.config/nvim/lua/estanislao/plugins/snacks.lua
@@ -4,6 +4,9 @@ return {
         priority = 1000,
         lazy = false,
         opts = {
+            indent = {
+                enabled = true,
+            },
             picker = {
                 enabled = true,
                 sources = {
