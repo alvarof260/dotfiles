84771028d98979930b8ff8d32b20590dcaf5076a
Alvaro Estanislao Figueroa <alvarof260@gmail.com>
2026-01-21 10:08:29 -0300

    feat: agregar resaltador de colores

--- nvim/.config/nvim/lua/estanislao/plugins/useful.lua
+++ nvim/.config/nvim/lua/estanislao/plugins/useful.lua
@@ -30,4 +30,13 @@ return {
 			},
 		},
 	},
+	{
+		"norcalli/nvim-colorizer.lua",
+		lazy = true,
+		config = function()
+			local colorizer = require("colorizer")
+
+			colorizer.setup({})
+		end,
+	},
 }
